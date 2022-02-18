09300fe6de1c7600246a123589ad2d984c3b7ab7
manhhung1509 <hung2332nguyenmanh@gmail.com>
2022-02-18 22:23:49 +0700

    upbai

--- case/src/main/java/com/example/demo/controller/HomeController.java
+++ case/src/main/java/com/example/demo/controller/HomeController.java
@@ -20,10 +20,12 @@ public class HomeController {
     public String searchInHome(@RequestParam String homeSearch) {
         String keySearch = homeSearch.toLowerCase();
 
-        if ("customer".equals(keySearch)){
+        if ("customer".equals(keySearch) || "customers".equals(keySearch)){
             return "redirect:/customers";
-        }else if ("employee".equals(keySearch)){
+        }else if ("employee".equals(keySearch) || "employees".equals(keySearch)){
             return "redirect:/employees";
+        }else if ("service".equals(keySearch) || "services".equals(keySearch)){
+            return "redirect:/services";
         }
         return "redirect:/home";
     }
